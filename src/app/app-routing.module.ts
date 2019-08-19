23bc75a45aa48c57959d301381115fc80c762ceb
Bartosz Rędziniak <bartosz.redziniak@great.com.pl>
2019-08-19 14:34:35 +0200

    Add redirection for mismatched path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
 		path: 'auth',
 		loadChildren: () => import('./feature/auth/auth.module').then(m => m.AuthModule),
 	},
+	{
+		path: '**',
+		redirectTo: '/',
+	},
 ];
 // tslint:enable:typedef
 
